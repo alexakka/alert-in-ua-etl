a35a7a6f0a176e55b59b5a7d69d1314fbb367282
alexakka <alex.akka7@gmail.com>
2025-03-29 12:37:10 +0200

    implemented load_active_alerts

--- dags/active_alerts.py
+++ dags/active_alerts.py
@@ -1,14 +1,15 @@
-from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.http.sensors.http import HttpSensor
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-from dotenv import load_dotenv
-import os
-import requests
 import json
-import pandas as pd
+import os
+from datetime import datetime
 
+import pandas as pd
+import requests
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -18,16 +19,20 @@ HEADERS = {
     "Accept": "application/json",
     "Authorization": f"Bearer {TOKEN}",
 }
-
 URL = "https://api.alerts.in.ua/v1/alerts/active.json"
 
-def get_active_alerts(ti):
+POSTGRES_CONN_ID = "postgres_connect"
+HTTP_CONN_ID = "alerts_api"
+
+
+def _extract_active_alerts(ti):
     response = requests.get(URL, headers=HEADERS)
     data = response.json()
 
     ti.xcom_push(key="active_alerts", value=data)
 
-def process_active_alerts(ti):
+
+def _transform_active_alerts(ti):
     data = ti.xcom_pull(key="active_alerts")
 
     processed_alerts = []
@@ -46,6 +51,15 @@ def process_active_alerts(ti):
     df_combined.to_csv("/tmp/processed_alerts.csv", index=False)
 
 
+def _load_active_alerts():
+    hook = PostgresHook(POSTGRES_CONN_ID)
+
+    sql ="COPY active_alerts FROM stdin WITH DELIMITER AS ',' CSV HEADER;"
+
+    hook.copy_expert(
+        sql=sql,
+        filename="/tmp/processed_alerts.csv"
+    )
 
 
 with DAG(
@@ -55,29 +69,40 @@ with DAG(
     catchup=False
 ) as dag:
 
-    first_task = PostgresOperator(
+    create_table = PostgresOperator(
         task_id="create_table",
-        postgres_conn_id="postgres_connect",
-        sql="SELECT 2 / 1;"
+        postgres_conn_id=POSTGRES_CONN_ID,
+        sql="""
+            CREATE TABLE IF NOT EXISTS active_alerts(
+                alert_id BIGINT,
+                location_title VARCHAR(255),
+                location_type VARCHAR(255),
+                alert_type VARCHAR(255),
+                started_at TIMESTAMP
+            );
+    """
     )
 
     is_alerts_api_available = HttpSensor(
         task_id="is_alerts_api_available",
-        http_conn_id="alerts_api",
+        http_conn_id=HTTP_CONN_ID,
         endpoint="alerts/active.json",
         headers=HEADERS
     )
 
     extract_active_alerts = PythonOperator(
         task_id="extract_active_alerts",
-        python_callable=get_active_alerts
+        python_callable=_extract_active_alerts
     )
 
-    processed_active_alerts = PythonOperator(
-        task_id="processed_active_alerts",
-        python_callable=process_active_alerts
+    transform_active_alerts = PythonOperator(
+        task_id="transform_active_alerts",
+        python_callable=_transform_active_alerts
     )
 
+    load_active_alerts = PythonOperator(
+        task_id="load_active_alerts",
+        python_callable=_load_active_alerts
+    )
 
-
-    first_task >> is_alerts_api_available >> extract_active_alerts >> processed_active_alerts
+    create_table >> is_alerts_api_available >> extract_active_alerts >> transform_active_alerts >> load_active_alerts
